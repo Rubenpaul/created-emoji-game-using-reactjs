1dd513520ce303f7f60c56ea40d3ba435d0b367c
Rubenpaul <94739637+Rubenpaul@users.noreply.github.com>
2022-11-26 23:27:46 +0530

    added changes to app

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -2,7 +2,22 @@
 import './index.css'
 
 const WinOrLoseCard = props => {
-  const {score} = props
+  const {topScore, score, playAgain} = props
+
+  let url
+  let resultText
+  if (score < 12) {
+    url = 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
+    resultText = 'You Lose'
+  } else if (score === 12) {
+    url = 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
+    resultText = 'You Win'
+  }
+
+  const onClickPlayAgain = () => {
+    playAgain()
+  }
+
   return (
     <div className="won-or-lose-container">
       <nav className="nav-container">
@@ -17,21 +32,21 @@ const WinOrLoseCard = props => {
       </nav>
       <div className="result-container">
         <div className="container">
-          <h1 className="match-result">You Won</h1>
+          <h1 className="match-result">{resultText}</h1>
           <p className="match-score">Best Score</p>
-          <span className="span">12/12</span>
+          <span className="span">{topScore}/12</span>
           <div className="play-again-container">
-            <button className="play-again-btn" type="button">
+            <button
+              className="play-again-btn"
+              type="button"
+              onClick={onClickPlayAgain}
+            >
               Play Again
             </button>
           </div>
         </div>
         <div className="image-container">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/won-game-img.png"
-            alt=""
-            className="match-result-image"
-          />
+          <img src={url} alt="" className="match-result-image" />
         </div>
       </div>
     </div>
