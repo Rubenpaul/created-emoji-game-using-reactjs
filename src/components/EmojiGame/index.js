889fbd538b39ed6e0c57c10c8499f4fb1fcf32ac
Rubenpaul <94739637+Rubenpaul@users.noreply.github.com>
2022-11-26 23:24:33 +0530

    added changes to app

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -2,7 +2,7 @@ import './index.css'
 
 import {Component} from 'react'
 
-import NavBar from '../NavBar/index'
+import NavBar from '../NavBar'
 
 import EmojiCard from '../EmojiCard'
 
@@ -23,9 +23,39 @@ const shuffledEmojisList = () => {
 // Write your code here.
 
 class EmojiGame extends Component {
-  state = {score: 0, totalscore: 0}
+  state = {score: 0, prevEmojiid: 0, topScore: 0}
+
+  playAgain = () => {
+    const {topScore} = this.state
+    console.log(topScore)
+  }
+
+  EmojiClicked = id => {
+    const {prevEmojiid, score, topScore} = this.state
+
+    if (prevEmojiid !== id) {
+      if (score < 12) {
+        this.setState(prevState => ({
+          prevEmojiid: id,
+          score: prevState.score + 1,
+          topScore: prevState.topScore + 1,
+        }))
+      }
+    } else {
+      this.setState(
+        this.setState(prevState => ({
+          score: prevState.score - 1,
+        })),
+      )
+    }
+
+    localStorage.setItem('prevTopScore', `${topScore}`)
+  }
 
   render() {
+    const {score} = this.state
+    console.log(score)
+
     const shuffledEmojisList = () => {
       const {emojisList} = this.props
       return emojisList.sort(() => Math.random() - 0.5)
@@ -35,15 +65,23 @@ class EmojiGame extends Component {
 
     return (
       <div className="app-container">
-        <NavBar />
-
-        <ul className="emoji-list-container">
-          {emojisList.map(eachEmoji => (
-            <EmojiCard EmojiDetails={eachEmoji} key={eachEmoji.id} />
-          ))}
-        </ul>
-
-        {/* <WinOrLoseCard /> */}
+        {score !== 0 && score === 12 ? (
+          <WinOrLoseCard score={score} playAgain={this.playAgain} />
+        ) : (
+          <>
+            <NavBar score={score} topScore={topScore} />
+            <ul className="emoji-list-container">
+              {emojisList.map(eachEmoji => (
+                <EmojiCard
+                  EmojiDetails={eachEmoji}
+                  key={eachEmoji.id}
+                  list={emojisList}
+                  EmojiClicked={this.EmojiClicked}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     )
   }
